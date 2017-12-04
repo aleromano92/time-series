5d2d09f64a09a4addc47268c3dc554311e96137d
Alessandro Romano <alessandro.romano@lastminute.com>
2017-12-04 16:18:11 +0100

    refactor index to use closures instead of prototype

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-function TimeSeries(db) {
-  this.db = db;
-}
+module.exports = db => {
 
-TimeSeries.prototype.addDataPoint = function(data, instant) {
-  return new Promise((resolve, reject) => {
-    this.db.collection('DataPoint').insertOne({ data, instant }, (err, result) => {
-      resolve(result.insertedId.toHexString());
+  function addDataPoint(data, instant) {
+    return new Promise((resolve, reject) => {
+      db.collection('DataPoint').insertOne({ data, instant }, (err, result) => {
+        resolve(result.insertedId.toHexString());
+      });
     });
-  });
-};
+  }
 
-TimeSeries.prototype.fetchDataPoint = function(code) {
-  return new Promise((resolve, reject) => {
-    this.db
-      .collection('DataPoint')
-      .findOne({ _id: new ObjectID(code) })
-      .then(mongoResult => {
-        const result = mongoResult;
-        resolve({ code: mongoResult._id, data: mongoResult.data, instant: mongoResult.instant });
-      })
-      .catch(console.log);
-  });
-};
+  function fetchDataPoint(code) {
+    return new Promise((resolve, reject) => {
+      db
+        .collection('DataPoint')
+        .findOne({ _id: new ObjectID(code) })
+        .then(mongoResult => {
+          const result = mongoResult;
+          resolve({ code: mongoResult._id, data: mongoResult.data, instant: mongoResult.instant });
+        })
+        .catch(console.log);
+    });
+  }
 
-module.exports = db => new TimeSeries(db);
+  return { addDataPoint, fetchDataPoint }
+};
